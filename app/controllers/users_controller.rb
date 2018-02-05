8d8d29d2ff8c9c6f4bdd76ef20b3a1d1549acead
N. Hutchison <blkfinch@gmail.com>
2018-02-05 11:11:16 -0700

    finished sign-in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.permission = 1;
+    if @user.save
+      flash[:success] = "Welcome to the forums!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :screen_name, :password, :password_confirmation, 
+                                  :email, :permission)
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202225209) do
+ActiveRecord::Schema.define(version: 20180205164800) do
 
   create_table "users", force: :cascade do |t|
     t.string "screen_name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20180202225209) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.integer "permission"
+    t.string "name"
   end
 
 end

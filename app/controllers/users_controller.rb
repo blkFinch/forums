af7bec2cea1b99eb1223bd4c960ef3d9eb10fa16
N. Hutchison <blkfinch@gmail.com>
2018-02-07 12:00:55 -0700

    finished user crud and permissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :show, :index]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :is_admin, only: [:index]
+  
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -19,8 +27,18 @@ class UsersController < ApplicationController
     end
   end
 
-  def index
-    @users = User.all
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:sucess] = "Profile Updated!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
@@ -29,4 +47,25 @@ class UsersController < ApplicationController
                                   :email, :permission)
     end
 
+    def logged_in_user
+      unless logged_in?
+        flash[:danger]  = "Please log in!"
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      unless @user == current_user || current_user.permission >= 3
+        flash[:danger] = "Not Authorized!"
+        redirect_to(root_url)
+      end
+    end
+
+    def is_admin
+      unless current_user.permission >= 3
+        flash[:danger] = "Not Authorized!"
+        redirect_to(root_url)
+      end
+    end
 end

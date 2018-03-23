187b46b0e57e32580053b28d2c66b5a1ac4491a8
N. Hutchison <blkfinch@gmail.com>
2018-03-23 11:49:16 -0600

    working on api call

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -2,7 +2,7 @@ class JsonWebToken
 
   HMAC_SECRET = Rails.application.secrets.secret_key_base
 
-  def self.encode(payload, exp = 24.hours.from_now)
+  def self.encode(payload, exp = 1.year.from_now)
     payload[:exp] = exp.to_i 
 
     JWT.encode(payload, HMAC_SECRET)
